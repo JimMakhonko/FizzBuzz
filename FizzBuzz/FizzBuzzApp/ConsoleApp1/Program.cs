e3051b2f4747d12c5c777fce96f6b7c1aea55b4d
Ievgenii Makhonko <yobaua@gmail.com>
2021-03-03 19:35:04 +0800

    FizzBuzz

---
+++ FizzBuzz/FizzBuzzApp/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number to loop through: ");
+            int inputNumber = int.Parse(Console.ReadLine());
+            for(int i = 1; i <= inputNumber; i++)
+            {
+                var output = "";
+
+                if( i % 3 == 0 )
+                {
+                    output += "Fizz";
+                }
+                if( i % 5 == 0 )
+                {
+                    output += "Buzz";
+                }
+                if(output == "")
+                {
+                    output = Convert.ToString(i);
+                }
+                Console.WriteLine(output);
+            }
+
+            //Waiting for user to acknowledge the result
+            Console.WriteLine("Press Enter to exit...");
+            Console.Read();
+        }
+    }
+}
